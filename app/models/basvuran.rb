d968fbda6c60f66cdb1be24f70cca7263652a574
Prelang Builder <builder@prelang.com>
2016-10-04 09:18:53 +0000

    Defined validation for attribute 'mail' on model 'Basvuran'

--- app/models/basvuran.rb
+++ app/models/basvuran.rb
@@ -2,4 +2,5 @@ class Basvuran < ActiveRecord::Base
   belongs_to :gonullu
   belongs_to :user
   validates_formatting_of :telefon, using: :us_phone
+  validates_formatting_of :mail, using: :email
 end
